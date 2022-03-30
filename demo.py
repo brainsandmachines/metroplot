20218267585f292e37f74d69768f165e2631b580
Tal Golan <golantl@gmail.com>
2022-03-30 12:07:00 -0400

    black formatting

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-
 from itertools import combinations
 
 import matplotlib.pyplot as plt
@@ -15,91 +14,133 @@ from metroplot import metroplot
 # Metroplot usage example
 
 # Read Eysenck (1974) table from David Howell's website
-data=pd.read_csv('https://www.uvm.edu/~statdhtx/methods8/DataFiles/Tab13-2.dat',delimiter='\t')
-data['Condition'] = [{1:'Counting', 2:'Rhyming', 3:'Adjective', 4:'Imagery', 5:'Intention'}[c] for c in data['Condition']]
-data['Age'] = [{1:'Old', 2:'Young'}[c] for c in data['Age']]
+data = pd.read_csv(
+    "https://www.uvm.edu/~statdhtx/methods8/DataFiles/Tab13-2.dat", delimiter="\t"
+)
+data["Condition"] = [
+    {1: "Counting", 2: "Rhyming", 3: "Adjective", 4: "Imagery", 5: "Intention"}[c]
+    for c in data["Condition"]
+]
+data["Age"] = [{1: "Old", 2: "Young"}[c] for c in data["Age"]]
+
 
 def pairwise_ttest(data, factor, dependent):
-    """ run t-tests between all levels of a factor and  """
-    levels=data[factor].unique()
+    """run t-tests between all levels of a factor and"""
+    levels = data[factor].unique()
 
     # test all pairwise comparisons
-    pairwise_comparisons=[]
-    for level1, level2 in combinations(levels,r=2):
-        a = data.loc[data[factor]==level1,dependent]
-        b = data.loc[data[factor]==level2,dependent]
+    pairwise_comparisons = []
+    for level1, level2 in combinations(levels, r=2):
+        a = data.loc[data[factor] == level1, dependent]
+        b = data.loc[data[factor] == level2, dependent]
 
-        t, p_value, df=sm.stats.ttest_ind(a,b)
+        t, p_value, df = sm.stats.ttest_ind(a, b)
 
-        pairwise_comparisons.append({
-            'level1':level1,
-            'level2':level2,
-            'effect_direction':np.sign(t),
-            'p_value':p_value,
-        })
-    pairwise_comparisons=pd.DataFrame(pairwise_comparisons)
+        pairwise_comparisons.append(
+            {
+                "level1": level1,
+                "level2": level2,
+                "effect_direction": np.sign(t),
+                "p_value": p_value,
+            }
+        )
+    pairwise_comparisons = pd.DataFrame(pairwise_comparisons)
 
     # multiple comparison correction
-    is_sig, corrected, _, _, = sm.stats.multipletests(pairwise_comparisons['p_value'], method='fdr_bh')
+    (
+        is_sig,
+        corrected,
+        _,
+        _,
+    ) = sm.stats.multipletests(pairwise_comparisons["p_value"], method="fdr_bh")
 
-    pairwise_comparisons['is_sig'] = is_sig
-    pairwise_comparisons['corrected_p_value'] = corrected # this is not currently used
+    pairwise_comparisons["is_sig"] = is_sig
+    pairwise_comparisons["corrected_p_value"] = corrected  # this is not currently used
 
     return pairwise_comparisons
 
-pairwise_comparisons = pairwise_ttest(data, 'Condition', 'Recall')
 
-conditions = data['Condition'].unique()
+pairwise_comparisons = pairwise_ttest(data, "Condition", "Recall")
 
-print('pairwise comparisons results:')
+conditions = data["Condition"].unique()
+
+print("pairwise comparisons results:")
 print(pairwise_comparisons)
 
-fig=plt.figure(figsize=(5,4))
-gs=GridSpec(2,1,height_ratios=[0.75,5],hspace=0.05,figure=fig) # we create two subplots, one for the main plot and one for the metroplot.
-ax0=fig.add_subplot(gs[1])
-sns.barplot(data=data,x='Condition',y='Recall',order=conditions)
+fig = plt.figure(figsize=(5, 4))
+gs = GridSpec(
+    2, 1, height_ratios=[0.75, 5], hspace=0.05, figure=fig
+)  # we create two subplots, one for the main plot and one for the metroplot.
+ax0 = fig.add_subplot(gs[1])
+sns.barplot(data=data, x="Condition", y="Recall", order=conditions)
 sns.despine(fig)
 gs.tight_layout(fig)
 
 # # grab colors from Seaborn's barplot.
-bars = [r for r in ax0.get_children() if type(r)==Rectangle]
+bars = [r for r in ax0.get_children() if type(r) == Rectangle]
 colors = [c.get_facecolor() for c in bars[:-1]]
-level_palette = {cond:color for cond, color in zip(conditions,colors)}
+level_palette = {cond: color for cond, color in zip(conditions, colors)}
 
-level_to_location = {cond:i for i, cond in enumerate(conditions)} # map categories to x axis locations.
-ax1=fig.add_subplot(gs[0])
-metroplot(pairwise_comparisons,level_to_location=level_to_location,metroplot_element_order=conditions,
-                                ax=ax1,level_axis='x',dominating_effect_direction=1,
-                                level_pallete=level_palette, level_axis_lim=ax0.get_xlim())
+level_to_location = {
+    cond: i for i, cond in enumerate(conditions)
+}  # map categories to x axis locations.
+ax1 = fig.add_subplot(gs[0])
+metroplot(
+    pairwise_comparisons,
+    level_to_location=level_to_location,
+    metroplot_element_order=conditions,
+    ax=ax1,
+    level_axis="x",
+    dominating_effect_direction=1,
+    level_pallete=level_palette,
+    level_axis_lim=ax0.get_xlim(),
+)
 # # note that level_axis_lim=ax0.get_xlim() aligns the metrplot with the main plot
 
 # %%
 # a slightly more complicated example
 
-fig=plt.figure(figsize=(6,6))
-gs=GridSpec(1,2,width_ratios=[4,2.5],wspace=0.01, figure=fig)
-ax0=fig.add_subplot(gs[0])
+fig = plt.figure(figsize=(6, 6))
+gs = GridSpec(1, 2, width_ratios=[4, 2.5], wspace=0.01, figure=fig)
+ax0 = fig.add_subplot(gs[0])
 
-data['Group']=[cond + ' ' + age for age, cond in zip(data['Age'],data['Condition'])]
-level_palette={'Counting':'#a6cee3', 'Rhyming':'#1f78b4', 'Adjective':'#b2df8a', 'Imagery':'#33a02c', 'Intention':'#fb9a99'}
+data["Group"] = [cond + " " + age for age, cond in zip(data["Age"], data["Condition"])]
+level_palette = {
+    "Counting": "#a6cee3",
+    "Rhyming": "#1f78b4",
+    "Adjective": "#b2df8a",
+    "Imagery": "#33a02c",
+    "Intention": "#fb9a99",
+}
 
-pairwise_comparisons = pairwise_ttest(data, 'Group', 'Recall')
+pairwise_comparisons = pairwise_ttest(data, "Group", "Recall")
 
-print('pairwise comparisons results:')
+print("pairwise comparisons results:")
 print(pairwise_comparisons)
 
-conditions = data['Group'].unique()
+conditions = data["Group"].unique()
 conditions.sort()
 
-sns.boxplot(data=data, y='Group',x='Recall', ax=ax0, color='w', order=conditions)#, hue='Condition', palette=level_palette)
+sns.boxplot(
+    data=data, y="Group", x="Recall", ax=ax0, color="w", order=conditions
+)  # , hue='Condition', palette=level_palette)
 gs.tight_layout(fig)
 
-level_to_location = {cond:i for i, cond in enumerate(conditions)} # map categories to y axis locations.
+level_to_location = {
+    cond: i for i, cond in enumerate(conditions)
+}  # map categories to y axis locations.
 
-ax1=fig.add_subplot(gs[1])
-metroplot(pairwise_comparisons,level_to_location=level_to_location,metroplot_element_order=conditions,
-                                ax=ax1, level_axis='y',dominating_effect_direction=1,
-                                level_pallete='k', level_axis_lim=ax0.get_ylim())
+ax1 = fig.add_subplot(gs[1])
+metroplot(
+    pairwise_comparisons,
+    level_to_location=level_to_location,
+    metroplot_element_order=conditions,
+    ax=ax1,
+    level_axis="y",
+    dominating_effect_direction=1,
+    level_pallete="k",
+    level_axis_lim=ax0.get_ylim(),
+)
 # # note that level_axis_lim=ax0.get_ylim() aligns the metrplot with the main plot
 
 plt.show()
--- metroplot.py
+++ metroplot.py
@@ -2,12 +2,24 @@ import warnings
 
 import matplotlib.pyplot as plt
 
-def metroplot(df, level_to_location, metroplot_element_order,
-                level_axis='y',dominating_effect_direction=1, ax=None,
-                level_pallete=None, level_axis_lim=None,
-                element_axis_lim=None, open_dot_fill_color='w',
-                marker='o', linewidth=0.5, markeredgewidth=0.5, markersize=8):
-    """ Plot a 'metroplot' pairwise comparisons significance plot.
+
+def metroplot(
+    df,
+    level_to_location,
+    metroplot_element_order,
+    level_axis="y",
+    dominating_effect_direction=1,
+    ax=None,
+    level_pallete=None,
+    level_axis_lim=None,
+    element_axis_lim=None,
+    open_dot_fill_color="w",
+    marker="o",
+    linewidth=0.5,
+    markeredgewidth=0.5,
+    markersize=8,
+):
+    """Plot a 'metroplot' pairwise comparisons significance plot.
 
     Each row in df should describe the outcome of one pair-wise comparison.
 
@@ -26,41 +38,49 @@ def metroplot(df, level_to_location, metroplot_element_order,
     """
 
     if ax is None:
-        ax =plt.gca()
+        ax = plt.gca()
 
     # eliminate comparisons between conditions that don't appear in level_to_location
-    df=df[df.level1.isin(level_to_location) | df.level2.isin(level_to_location)]
+    df = df[df.level1.isin(level_to_location) | df.level2.isin(level_to_location)]
 
     # eliminate non-significant comparisons
-    df=df[df.is_sig]
+    df = df[df.is_sig]
 
-    cur_line_x=0
+    cur_line_x = 0
     for dominating_level in metroplot_element_order:
 
         if dominating_level is None:
             continue
 
         # find dominated levels
-        row_filter = (df.level1 == dominating_level) & (df.level2.isin(level_to_location)) & (df['effect_direction']==dominating_effect_direction)
+        row_filter = (
+            (df.level1 == dominating_level)
+            & (df.level2.isin(level_to_location))
+            & (df["effect_direction"] == dominating_effect_direction)
+        )
         dominated_levels_list = list(df[row_filter].level2)
-        row_filter = (df.level2 == dominating_level) & (df.level1.isin(level_to_location)) & (df['effect_direction']==-dominating_effect_direction)
+        row_filter = (
+            (df.level2 == dominating_level)
+            & (df.level1.isin(level_to_location))
+            & (df["effect_direction"] == -dominating_effect_direction)
+        )
         dominated_levels_list.extend(list(df[row_filter].level1))
 
-        if len(dominated_levels_list)==0:
+        if len(dominated_levels_list) == 0:
             continue
 
         # the following notation assumes the level_axis is y.
-        x=[]
-        y=[]
-        c_fill=[]
-        c_edge=[]
+        x = []
+        y = []
+        c_fill = []
+        c_edge = []
 
-        if isinstance(level_pallete,dict):
-            element_color=level_pallete[dominating_level]
+        if isinstance(level_pallete, dict):
+            element_color = level_pallete[dominating_level]
         elif level_pallete is not None:
             element_color = level_pallete
         else:
-            element_color='k'
+            element_color = "k"
 
         # add points to represent the dominated level
         for dominated_level in dominated_levels_list:
@@ -75,39 +95,54 @@ def metroplot(df, level_to_location, metroplot_element_order,
         c_fill.append(element_color)
         c_edge.append(element_color)
 
-        cur_line_x+=1
+        cur_line_x += 1
 
-        if level_axis.lower() == 'y':
+        if level_axis.lower() == "y":
             pass
-        elif level_axis.lower() == 'x':
-            x, y = y, x # flip axes.
+        elif level_axis.lower() == "x":
+            x, y = y, x  # flip axes.
         else:
-            raise ValueError('level_axis must be x or y.')
+            raise ValueError("level_axis must be x or y.")
 
-        ax.plot(x,y,'-',color=element_color,clip_on=False,linewidth=linewidth) # plot line
-        for i in range(len(x)): # plot dots
-            ax.plot(x[i],y[i],marker,markerfacecolor=c_fill[i],markeredgecolor=c_edge[i],clip_on=False,markeredgewidth=markeredgewidth, markersize=markersize) # plot markers
+        ax.plot(
+            x, y, "-", color=element_color, clip_on=False, linewidth=linewidth
+        )  # plot line
+        for i in range(len(x)):  # plot dots
+            ax.plot(
+                x[i],
+                y[i],
+                marker,
+                markerfacecolor=c_fill[i],
+                markeredgecolor=c_edge[i],
+                clip_on=False,
+                markeredgewidth=markeredgewidth,
+                markersize=markersize,
+            )  # plot markers
 
     if level_axis_lim is not None:
-        if level_axis.lower() == 'y':
+        if level_axis.lower() == "y":
             ax.set_ylim(level_axis_lim)
         else:
             ax.set_xlim(level_axis_lim)
 
     if element_axis_lim is not None:
-        if level_axis.lower() == 'y':
-            cur_ax=ax.get_xlim()
+        if level_axis.lower() == "y":
+            cur_ax = ax.get_xlim()
             set_func = ax.set_xlim
         else:
-            cur_ax=ax.get_ylim()
+            cur_ax = ax.get_ylim()
             set_func = ax.set_ylim
         if cur_ax[0] < element_axis_lim[0]:
-            warnings.warn(f"element_axis_lim[0] too small ({cur_ax[0]} < {element_axis_lim[0]}) adjusting to avoid clipped elements.")
-            element_axis_lim[0]=cur_ax[0]
+            warnings.warn(
+                f"element_axis_lim[0] too small ({cur_ax[0]} < {element_axis_lim[0]}) adjusting to avoid clipped elements."
+            )
+            element_axis_lim[0] = cur_ax[0]
         if cur_ax[1] > element_axis_lim[1]:
-            warnings.warn(f"element_axis_lim[1] too small ({cur_ax[1]} > {element_axis_lim[1]}) adjusting to avoid clipped elements.")
-            element_axis_lim[1]=cur_ax[1]
+            warnings.warn(
+                f"element_axis_lim[1] too small ({cur_ax[1]} > {element_axis_lim[1]}) adjusting to avoid clipped elements."
+            )
+            element_axis_lim[1] = cur_ax[1]
 
         set_func(element_axis_lim)
 
-    ax.axis('off')
+    ax.axis("off")
